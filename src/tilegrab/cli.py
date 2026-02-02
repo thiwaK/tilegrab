7e992afcee51088bfad6f3a293727985e02aaf37
Thiwanka Kaushal <thiwanka.kaushal.mob@gmail.com>
2026-02-02 20:33:57 +0530

    seperate logging logics from cli.py

--- src/tilegrab/cli.py
+++ src/tilegrab/cli.py
@@ -1,91 +1,16 @@
 #!/usr/bin/env python3
 import logging
 import argparse
-import random
-import sys
 from pathlib import Path
 from tilegrab.downloader import Downloader
 from tilegrab.images import TileImageCollection
+from tilegrab.logs import setup_logging
 from tilegrab.tiles import TilesByShape, TilesByBBox
 from tilegrab.dataset import GeoDataset
 from tilegrab import version
 
-# Configure root logger
-# logging.basicConfig(
-#     level=logging.INFO,
-#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-#     handlers=[
-#         logging.StreamHandler(sys.stdout),
-#         logging.FileHandler('tilegrab.log')
-#     ]
-# )
-
-# Normal colors
-BLACK   = "\033[30m"
-RED     = "\033[31m"
-GREEN   = "\033[32m"
-YELLOW  = "\033[33m"
-BLUE    = "\033[34m"
-MAGENTA = "\033[35m"
-CYAN    = "\033[36m"
-GRAY    = "\033[90m"
-WHITE  = "\033[37m"
-
-# Bright colors
-BBLACK   = "\033[90m"
-BRED     = "\033[91m"
-BGREEN   = "\033[92m"
-BYELLOW  = "\033[93m"
-BBLUE    = "\033[94m"
-BMAGENTA = "\033[95m"
-BCYAN    = "\033[96m"
-BGRAY    = "\033[97m"
-BWHITE = "\033[97m"
-
-RESET = "\033[0m"
-
-
-class LogFormatter(logging.Formatter):
-    NAME_WIDTH = 14
-
-    LEVEL_MAP = {
-        logging.CRITICAL: f'{RED}‼ {RESET}',
-        logging.ERROR:    f'{RED}✖ {RESET}',
-        logging.WARNING:  f'{YELLOW}⚠ {RESET}',
-        logging.INFO:     f'{BLUE}• {RESET}',
-        logging.DEBUG:    f'{GRAY}· {RESET}',
-        logging.NOTSET:   f'{CYAN}- {RESET}',
-    }
-
-    def format(self, record):
-        record.level_letter = self.LEVEL_MAP.get(record.levelno, '?')
-
-        short = record.name.rsplit('.', 1)[-1]
-        record.short_name = f"{short:<{self.NAME_WIDTH}}"
-
-        return super().format(record)
-
-
-console_formatter = LogFormatter(
-    f'   %(level_letter)s %(message)s'
-)
-file_formatter = logging.Formatter(
-    '%(asctime)s %(levelname)s %(name)s - %(message)s'
-)
-
-console = logging.StreamHandler(sys.stdout)
-console.setFormatter(console_formatter )
-
-file = logging.FileHandler('tilegrab.log')
-file.setFormatter(file_formatter)
-
-logging.basicConfig(
-    level=logging.INFO,
-    handlers=[console, file],
-)
 
 logger = logging.getLogger(__name__)
-
 
 def parse_args() -> argparse.Namespace:
     p = argparse.ArgumentParser(
@@ -148,56 +73,47 @@ def parse_args() -> argparse.Namespace:
     p.add_argument(
         "--no-progress", action="store_false", help="Hide download progress bar"
     )
+    p.add_argument("--quiet", action="store_true", help="Hide all prints")
+    p.add_argument("--debug", action="store_true", help="Enable debug logging")
     p.add_argument(
-        "--quiet", action="store_true", help="Hide all prints"
-    )
-    p.add_argument(
-        "--debug", action="store_true", help="Enable debug logging"
-    )
-    p.add_argument(
-        "--test", action="store_true", help="Only for testing purposes, not for normal use"
+        "--test",
+        action="store_true",
+        help="Only for testing purposes, not for normal use",
     )
 
     return p.parse_args()
 
 
 def main():
+    LOG_LEVEL = logging.INFO
+    ENABLE_CLI_LOG = True
+    ENABLE_FILE_LOG = True
+
     args = parse_args()
-    
-    # Adjust logging level
     if args.debug:
-        logging.getLogger().setLevel(logging.DEBUG)
-        # logger.debug("Debug logging enabled")
-    elif args.quiet:
-        console.close()
-        
+        LOG_LEVEL = logging.DEBUG
+    if args.quiet:
+        ENABLE_CLI_LOG = False
     
-    
-    BANNER_NORMAL, BANNER_BRIGHT = random.choice([
-        (RED, BRED),
-        (GREEN, BGREEN),
-        (YELLOW, BYELLOW),
-        (BLUE, BBLUE),
-        (MAGENTA, BMAGENTA),
-        (CYAN, BCYAN),
-        (GRAY, BGRAY),
-    ])
+    setup_logging(ENABLE_CLI_LOG, ENABLE_FILE_LOG, LOG_LEVEL)
 
     if not args.quiet:
         print()
-        print(f"{WHITE}   " + ("-" * 60) + f"{RESET}")
-        print(f"{BWHITE}  TileGrab v{version}{RESET}".rjust(50))
-        print(f"{WHITE}   " + ("-" * 60) + f"{RESET}")
-    
+        print(f"\033[37m   " + ("-" * 60) + "\033[0m")
+        print(f"\033[97m  TileGrab v{version}\033[0m".rjust(50))
+        print(f"\033[37m   " + ("-" * 60) + "\033[0m")
+
     try:
         dataset = GeoDataset(args.source)
         logger.info(f"Dataset loaded successfully from {args.source}")
 
         _tmp = "bbox" if args.bbox else "shape" if args.shape else "DnE"
-        logger.info(f"""Downloading tiles using {_tmp}
+        logger.info(
+            f"""Downloading tiles using {_tmp}
         - minX: {dataset.bbox.minx:.4f}     - minY: {dataset.bbox.miny:.4f}
         - maxX: {dataset.bbox.maxx:.4f}     - maxY: {dataset.bbox.maxy:.4f}
-        - zoom: {args.zoom}""")
+        - zoom: {args.zoom}"""
+        )
 
         if args.shape:
             tiles = TilesByShape(dataset, zoom=args.zoom)
@@ -210,34 +126,39 @@ def main():
         # Choose source provider
         if args.osm:
             from tilegrab.sources import OSM
+
             logger.info("Using OpenStreetMap (OSM) as tile source")
             source = OSM(api_key=args.key) if args.key else OSM()
         elif args.google_sat:
             from tilegrab.sources import GoogleSat
+
             logger.info("Using Google Satellite as tile source")
             source = GoogleSat(api_key=args.key) if args.key else GoogleSat()
         elif args.esri_sat:
             from tilegrab.sources import ESRIWorldImagery
+
             logger.info("Using ESRI World Imagery as tile source")
-            source = ESRIWorldImagery(api_key=args.key) if args.key else ESRIWorldImagery()
+            source = (
+                ESRIWorldImagery(api_key=args.key) if args.key else ESRIWorldImagery()
+            )
         else:
             logger.error("No tile source selected")
             raise SystemExit("No tile source selected")
-        
+
         downloader = Downloader(tiles, source, args.out)
         result: TileImageCollection
 
         if args.mosaic_only:
             result = TileImageCollection(args.out)
-            result.load(tiles)  
+            result.load(tiles)
         else:
             result = downloader.run(show_progress=args.no_progress)
             logger.info(f"Download result: {result}")
-        
-        if not args.download_only: result.mosaic()
+
+        if not args.download_only:
+            result.mosaic()
         logger.info("Done")
 
-        
     except Exception as e:
         logger.exception("Fatal error during execution")
         raise SystemExit(1)
---
+++ src/tilegrab/logs.py
@@ -0,0 +1,85 @@
+import logging
+import sys
+
+# Normal colors
+BLACK   = "\033[30m"
+RED     = "\033[31m"
+GREEN   = "\033[32m"
+YELLOW  = "\033[33m"
+BLUE    = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN    = "\033[36m"
+GRAY    = "\033[90m"
+WHITE  = "\033[37m"
+
+# Bright colors
+BBLACK   = "\033[90m"
+BRED     = "\033[91m"
+BGREEN   = "\033[92m"
+BYELLOW  = "\033[93m"
+BBLUE    = "\033[94m"
+BMAGENTA = "\033[95m"
+BCYAN    = "\033[96m"
+BGRAY    = "\033[97m"
+BWHITE = "\033[97m"
+
+RESET = "\033[0m"
+
+
+class CLILogFormatter(logging.Formatter):
+    NAME_WIDTH = 20
+    LEVEL_MAP = {
+        logging.CRITICAL: f'{RED}‼ {RESET}',
+        logging.ERROR:    f'{RED}✖ {RESET}',
+        logging.WARNING:  f'{YELLOW}⚠ {RESET}',
+        logging.INFO:     f'{BLUE}• {RESET}',
+        logging.DEBUG:    f'{GRAY}· {RESET}',
+        logging.NOTSET:   f'{CYAN}- {RESET}',
+    }
+
+    def __init__(self, fmt=None):
+        super().__init__(fmt or '   %(level_icon)s %(message)s')
+
+    def format(self, record):
+        record.level_letter = self.LEVEL_MAP.get(record.levelno, '?')
+
+        short = record.name.rsplit('.', 1)[-1]
+        record.short_name = f"{short:<{self.NAME_WIDTH}}"
+
+        return super().format(record)
+
+class FileLogFormatter(logging.Formatter):
+    def __init__(self):
+        super().__init__(
+            '%(asctime)s %(levelname)s %(name)s - %(message)s'
+        )
+
+def create_cli_handler(level=logging.INFO):
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(level)
+    handler.setFormatter(CLILogFormatter())
+    return handler
+
+def create_file_handler(path="tilegrab.log", level=logging.DEBUG):
+    handler = logging.FileHandler(path)
+    handler.setLevel(level)
+    handler.setFormatter(FileLogFormatter())
+    return handler
+
+def setup_logging(
+    enable_cli=True,
+    enable_file=True,
+    level=logging.INFO
+):
+    handlers = []
+
+    if enable_cli:
+        handlers.append(create_cli_handler(level))
+
+    if enable_file:
+        handlers.append(create_file_handler(level=level))
+
+    logging.basicConfig(
+        level=level,
+        handlers=handlers,
+    )
